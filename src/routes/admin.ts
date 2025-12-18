3c7aa37efb6c192daab49cd6514efbe4119b1083
mdsac1441 <amdahmed0@gmail.com>
2025-12-18 14:04:04 +0530

    fixed router

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -4,9 +4,7 @@ import { isAdmin, protect } from '../middleware'
 
 const adminRoutes = new Hono();
 
-adminRoutes.post('/create', (c)=> admin.createAdmin(c));  // Create Admin (Only One)
-adminRoutes.post('/login', (c)=> admin.loginAdmin(c));    // Login Admin
-adminRoutes.get('/profile', protect, isAdmin, (c)=> admin.getAdmin(c));     // Get Admin Profile
+adminRoutes.post('/create', (c)=> admin.createAdmin(c));  // Create Admin (Only One)  // Login Admin
 adminRoutes.put('/update', protect, isAdmin, (c)=> admin.updateAdmin(c));   // Update Admin Details
 
 export default adminRoutes;
